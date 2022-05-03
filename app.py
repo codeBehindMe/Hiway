340fe7a723d7165b558eaac6a6c65fa183a430d9
codeBehindMe <aaron.tillekeratne@gmail.com>
2022-05-03 23:49:07 +0000

    Added header

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from requests import head
 import streamlit as st
-
+from src.containers.header import header
 
 if __name__ == "__main__":
-    st.header("Welcome to hiway")
+    header()
---
+++ src/containers/header.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def header():
+
+  st.header("Hi Way")
