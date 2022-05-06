aaabea0ba8d65807a68093d8bb2346aadc0b2284
codeBehindMe <aaron.tillekeratne@gmail.com>
2022-05-06 00:19:57 +0000

    Added wide page config

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from src.containers.header import header
+from src.containers.journey_planner import journey_planner
 import os
 import plotly.express as px
 
 
 px.set_mapbox_access_token(os.environ["MAPBOX_TOKEN"])
+st.set_page_config(layout="wide")
 
 
 if __name__ == "__main__":
     header()
+    journey_planner()
