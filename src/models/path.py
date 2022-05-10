8c17ca77ecfb35afdb6738029234f4381f239c38
codeBehindMe <aaron.tillekeratne@gmail.com>
2022-05-10 09:28:52 +0000

    Added methods to segments

--- src/models/path.py
+++ src/models/path.py
@@ -14,6 +14,23 @@ class Segment:
     START: Point
     END: Point
 
+    def to_dataframe(self) -> pd.DataFrame:
+        return pd.DataFrame(
+            [(self.START.LAT, self.START.LNG), (self.END.LAT, self.END.LNG)]
+        )
+
+    def extreme_left(self) -> float:
+        return min(self.START.LNG, self.END.LNG)
+
+    def extreme_right(self) -> float:
+        return max(self.START.LNG, self.END.LNG)
+
+    def extreme_top(self) -> float:
+        return min(self.START.LAT, self.END.LAT)
+
+    def extreme_bottom(self) -> float:
+        return max(self.START.LAT, self.END.LAT)
+
 
 class Path:
     def __init__(self, segments: List[Segment]) -> None:
